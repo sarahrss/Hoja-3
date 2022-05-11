c8d249cb508071199c77710a3b84dc8532b5add8
sarahrss <sarahruiz@ufm.edu>
2022-05-10 22:13:55 -0600

    Create Bubble sort.py

---
+++ Sorting/Bubble sort.py
@@ -0,0 +1,10 @@
+def bubble_sort(arr):
+
+    for i in range(len(arr)):
+        for j in range(len(arr) - 1):
+            if arr[j] > arr[j + 1]:
+                arr[j], arr[j + 1] = arr[j + 1], arr[j]
+    
+    return arr
+list=[4,1,9,8]
+print(bubble_sort(list))
