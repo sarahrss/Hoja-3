899e6fbcf6daf78116b66187caaff6c801b89645
sarahrss <sarahruiz@ufm.edu>
2022-05-10 21:56:28 -0600

    Create Sum of first n numbers.py

---
+++ Brute force/Sum of first n numbers.py
@@ -0,0 +1,9 @@
+import sys
+sys.setrecursionlimit(2000) 
+def sum (nparam):
+    n=nparam
+    if (n == 1): 
+        return 1
+    else: ##recursive case 
+        return n + sum(n-1)
+print(sum(1000))
